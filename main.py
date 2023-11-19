5cfdae44929a7e646e3bcb748e2562d3c20c631b
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-11-19 19:32:54 +0100

    NaN

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from typing import Tuple
 
 import numpy as np
-import tkinter
+import tkinter as tk
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from marinetrafficapi import MarineTrafficApi
@@ -10,8 +10,16 @@ import asyncio
 import websockets
 import json
 from datetime import datetime, timezone
+import common_use_modules as cum
 
 APIKEY = os.environ['AISAPIKEY']
+
+gps_route = []
+kalman_route = []
+physics_route = []
+
+window = tk.Tk()
+window.title("Kalman Filter for ships")
 
 
 async def connect_ais_stream():
@@ -24,6 +32,7 @@ async def connect_ais_stream():
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
 
+
         async for message_json in websocket:
             message = json.loads(message_json)
             message_type = message["MessageType"]
@@ -35,6 +44,8 @@ async def connect_ais_stream():
                       f"Latitude: {ais_message['Latitude']} Longitude: {ais_message['Longitude']} "
                       f"SOG: {ais_message['Sog']} COG: {ais_message['Cog']}")
 
+                gps_route.append([ais_message['Latitude'], ais_message['Longitude']])
+                draw_map(gps_route, kalman_route, physics_route)
 
 test_coords = [[0.0, 0.0], [12.0, -10.0], [15.0, -9.0], [25.0, -8.0], [30.0, -7.0]]
 
@@ -43,11 +54,11 @@ def knots_to_mps(knots) -> float:
     return knots * 0.5144
 
 
-def projection_size() -> tuple[float, float, float, float]:
-    min_lat = min([x[1] for x in test_coords])
-    max_lat = max([x[1] for x in test_coords])
-    min_lon = min([x[0] for x in test_coords])
-    max_lon = max([x[0] for x in test_coords])
+def projection_size(route) -> tuple[float, float, float, float]:
+    min_lat = min([x[1] for x in route])
+    max_lat = max([x[1] for x in route])
+    min_lon = min([x[0] for x in route])
+    max_lon = max([x[0] for x in route])
 
     # delta_lat = max_lat - min_lat
     # delta_lon = max_lon - min_lat
@@ -60,14 +71,17 @@ def projection_size() -> tuple[float, float, float, float]:
     min_lon -= delta * margin
     max_lon += delta * margin
 
+    min_lat = cum.clamp(min_lat, -90, 90)
+    max_lat = cum.clamp(max_lat, -90, 90)
+
     return min_lat, max_lat, min_lon, max_lon
 
 
-def draw_map():
+def draw_map(gps_route, kalman_route, physics_route):
     # set up orthographic map projection with
     # perspective of satellite looking down at 50N, 100W.
     # use low resolution coastlines.
-    min_lat, max_lat, min_lon, max_lon = projection_size()
+    min_lat, max_lat, min_lon, max_lon = projection_size(gps_route)
     map = Basemap(projection='cea', llcrnrlat=min_lat, urcrnrlat=max_lat,
                   llcrnrlon=min_lon,urcrnrlon=max_lon, resolution='l')
     # draw coastlines, country boundaries, fill continents.
@@ -92,16 +106,16 @@ def draw_map():
     # x, y = map(lons * 180. / np.pi, lats * 180. / np.pi)
     # contour data over the map.
     # map.contour(x, y, wave + mean, 15, linewidths=1.5)
-    for i in range(0, len(test_coords) - 2):
-        map.drawgreatcircle(test_coords[i][0], test_coords[i][1], test_coords[i+1][0],
-                            test_coords[i+1][1], linewidth=1.5, color='r')
+    for i in range(0, len(gps_route) - 2):
+        map.drawgreatcircle(gps_route[i][0], gps_route[i][1], gps_route[i+1][0],
+                            gps_route[i+1][1], linewidth=1.5, color='r')
     plt.title('Kalman Filter')
     plt.show()
 
 
 def main():
-    # asyncio.run(asyncio.run(connect_ais_stream()))
-    draw_map()
+    asyncio.run(asyncio.run(connect_ais_stream()))
+    # draw_map()
 
 
 if __name__ == '__main__':
