902a15727f89622ff08be32dfabdf39cd07b7e77
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-11-19 20:03:46 +0100

    feat: Checkboxes

--- frontend.py
+++ frontend.py
@@ -1,7 +1,37 @@
 import tkinter as tk
 
+
+def on_click():
+    if draw_measured.get():
+        print("owo")
+    if draw_kalman.get():
+        print("uwu")
+    if draw_physical.get():
+        print("nya")
+
+
 if __name__ == '__main__':
-    root = tk.Tk(screenName="h")
-    title = tk.Label(root, text="Kalman's Filter")
+    root = tk.Tk(screenName="kalman")
+    title = tk.Label(root, text="Kalman's Filter for Ships")
     title.pack()
+
+    draw_measured = tk.BooleanVar()
+    draw_kalman = tk.BooleanVar()
+    draw_physical = tk.BooleanVar()
+
+    ck1 = tk.Checkbutton(root, text='Draw Measured', variable=draw_measured, onvalue=True, offvalue=False,
+                         command=on_click)
+    ck1.pack()
+
+    ck2 = tk.Checkbutton(root, text='Draw Kalman Prediction', variable=draw_kalman, onvalue=True, offvalue=False,
+                         command=on_click)
+    ck2.pack()
+
+    ck3 = tk.Checkbutton(root, text='Draw Physics-Based', variable=draw_physical, onvalue=True, offvalue=False,
+                         command=on_click)
+    ck3.pack()
+
+    id_textbox = tk.Text(root, height=8)
+    id_textbox.pack()
+
     root.mainloop()
--- main.py
+++ main.py
@@ -41,8 +41,9 @@ async def connect_ais_stream():
                       f"Latitude: {ais_message['Latitude']} Longitude: {ais_message['Longitude']} "
                       f"SOG: {ais_message['Sog']} COG: {ais_message['Cog']}")
 
-                gps_route.append([ais_message['Latitude'], ais_message['Longitude']])
+                gps_route.append([ais_message['Longitude'], ais_message['Latitude']])
                 draw_map(gps_route, kalman_route, physics_route)
+
 
 test_coords = [[0.0, 0.0], [12.0, -10.0], [15.0, -9.0], [25.0, -8.0], [30.0, -7.0]]
 
