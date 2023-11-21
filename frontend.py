3e3f30e5dce08a2927b8a1ea29ba4d49b8b37a2b
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-11-21 10:54:24 +0100

    minor changes

--- frontend.py
+++ frontend.py
@@ -28,40 +28,23 @@ def draw_map(gps_route, kalman_route, physics_route):
     min_lat, max_lat, min_lon, max_lon = projection_size(gps_route)
     map = Basemap(projection='cyl', llcrnrlat=min_lat, urcrnrlat=max_lat,
                   llcrnrlon=min_lon,urcrnrlon=max_lon, resolution='h')
+
     # draw coastlines, country boundaries, fill continents.
     map.drawcoastlines(linewidth=0.25)
     map.drawcountries(linewidth=0.25)
     map.fillcontinents(color='coral', lake_color='aqua')
+
     # draw the edge of the map projection region (the projection limb)
     map.drawmapboundary(fill_color='aqua')
-    # map.drawgreatcircle(xx, yy,linewidth=1.5,color='r')
+
     # draw lat/lon grid lines every 30 degrees.
     map.drawmeridians(np.arange(0, 360, 30))
     map.drawparallels(np.arange(-90, 90, 30))
-    # make up some data on a regular lat/lon grid.
-    # nlats = 73
-    # nlons = 145
-    # delta = 2. * np.pi / (nlons - 1)
-    # lats = (0.5 * np.pi - delta * np.indices((nlats, nlons))[0, :, :])
-    # lons = (delta * np.indices((nlats, nlons))[1, :, :])
-    # wave = 0.75 * (np.sin(2. * lats) ** 8 * np.cos(4. * lons))
-    # mean = 0.5 * np.cos(2. * lats) * ((np.sin(2. * lats)) ** 2 + 2.)
-    # compute native map projection coordinates of lat/lon grid.
-    # x, y = map(lons * 180. / np.pi, lats * 180. / np.pi)
-    # contour data over the map.
-    # map.contour(x, y, wave + mean, 15, linewidths=1.5)
 
     gps_x = [row[0] for row in gps_route]
     gps_y = [row[1] for row in gps_route]
     map.plot(gps_x, gps_y, linewidth=1.5, color='r')
-    map.scatter([p[0] for p in gps_route], [p[1] for p in gps_route], marker='o', color='r', s=6)
-
-    # print(gps_route)
-    # gps_route = [[4.52, 51.83], [4.52, 51.83]]
-    # for i in range(0, len(gps_route) - 1):
-    #     print(gps_route[i][0], gps_route[i][1], gps_route[i+1][0], gps_route[i+1][1])
-    #     map.drawgreatcircle(gps_route[i][0], gps_route[i][1], gps_route[i+1][0],
-    #                         gps_route[i+1][1], linewidth=1.5, color='r')
+    map.scatter([p[0] for p in gps_route], [p[1] for p in gps_route], marker=(), color='r', s=6)
 
     plt.title('Kalman Filter')
     plt.show()
